da458cd041955ce30cfa58f07291e06a45a95601
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-10-07 21:35:12 +1300

    init

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Dtos;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ItemsController : ControllerBase
+	{
+		private readonly IItemsRepository _repository;
+
+		public ItemsController(IItemsRepository repository) {
+			_repository = repository;
+		}
+
+	  // GET /items
+		[HttpGet]
+		public IEnumerable<ItemDto> GetItems()
+		{
+				var items = _repository.GetItems().Select(item => item.ToItemDto());
+				return items;
+		}
+
+		// GET /items/id
+		[HttpGet("{id}")]
+		public ActionResult<ItemDto> GetItem(Guid id)
+		{
+			var item = _repository.GetItem(id);
+
+			if (item is null)
+			{
+				return NotFound();
+			}
+
+			return Ok(item.ToItemDto());
+		}
+	}
+}
